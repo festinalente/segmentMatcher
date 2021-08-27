c42ea0a5651c385a4a4b317f7cc7b6ea773ac28f
festinalente <tomas@swiftmo.com>
2021-08-27 10:57:33 +0100

    rm token

--- fetchSegments.js
+++ fetchSegments.js
@@ -25,7 +25,7 @@ module.exports = (swpt, nept)=>{
   function getSegments(i, squares){
 
     const url = `https://www.strava.com/api/v3/segments/explore?bounds=${squares[i][0].toString()},${squares[i][1].toString()}
-    &activity_type=riding&access_token=c8735588199aff8001f79f4f9faa90ecb4424300`;
+    &activity_type=riding&access_token=`;
 
     getPolylines(url).then(makeGPX).then(() =>{
       setTimeout(()=>{
